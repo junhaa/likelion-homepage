78f912a595938628090be4053f29106d9c159c2e
junhaa <2171326@hansung.ac.kr>
2024-05-10 02:19:01 +0900

    :sparkles: FEAT. 공통 응답 DTO 테스트를 위한 컨트롤러 작성

--- src/main/java/com/homepage/likelion/test/CustomTestController.java
+++ src/main/java/com/homepage/likelion/test/CustomTestController.java
@@ -2,6 +2,7 @@ package com.homepage.likelion.test;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,13 @@ public class CustomTestController {
 	@PostMapping("/simple")
 	public CustomApiResponse<?> simple(){
 		return CustomApiResponse.createSuccess(HttpStatus.OK.value(), null, "회원 가입에 성공했습니다.");
+	}
+
+	@GetMapping("/jsonData")
+	public CustomApiResponse<SimpleDto> jsonData(){
+		return CustomApiResponse.createSuccess(SimpleDto.builder()
+			.email("2171326@hansung.ac.kr")
+			.userId("junhaa")
+			.build());
 	}
 }
---
+++ src/main/java/com/homepage/likelion/test/SimpleDto.java
@@ -0,0 +1,16 @@
+package com.homepage.likelion.test;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleDto {
+	private String userId;
+	private String email;
+}
